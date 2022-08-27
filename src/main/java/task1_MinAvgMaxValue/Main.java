31025828b027deefdc9d226d892a27b4559915d0
Anatolii Vasilev <avas2012@gmail.com>
2022-08-27 15:59:41 +0400

    created branch develop and added folder task1

---
+++ src/main/java/task1_MinAvgMaxValue/Main.java
@@ -0,0 +1,15 @@
+package task1_MinAvgMaxValue;
+
+    /*
+   Заполните массив случайным числами и выведите максимальное,
+   минимальное и среднее значение.
+    */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+}
+
---
+++ src/main/java/task1_SortArray/Main.java
@@ -0,0 +1,16 @@
+package task1_SortArray;
+
+/*
+Отсортируйте массив [5,6,3,2,5,1,4,9]
+ */
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+}
